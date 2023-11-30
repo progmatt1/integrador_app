a36df5324fa72fb40b3861e00aa44bff436f7c52
progmatt1 <93005834+progmatt1@users.noreply.github.com>
2023-11-29 21:00:03 -0300

    edit alumno

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def main(page: ft.Page):
     apellidoField = ft.TextField(text_align=ft.TextAlign.RIGHT, width=150, hint_text="Apellido")
     promedioField = ft.TextField(text_align=ft.TextAlign.RIGHT, width=150, hint_text="Promedio")
     cursoField = ft.TextField(text_align=ft.TextAlign.RIGHT, width=150, hint_text="Curso")
+   
+    dniFieldEdit = ft.TextField(text_align=ft.TextAlign.RIGHT, width=150, hint_text="DNI del usuario a editar")
+    nombreFieldEdit = ft.TextField(text_align=ft.TextAlign.RIGHT, width=150, hint_text="Nombre Nuevo")
+    apellidoFieldEdit = ft.TextField(text_align=ft.TextAlign.RIGHT, width=150, hint_text="Apellido Nuevo")
+    promedioFieldEdit = ft.TextField(text_align=ft.TextAlign.RIGHT, width=150, hint_text="Promedio Nuevo")
+    cursoFieldEdit = ft.TextField(text_align=ft.TextAlign.RIGHT, width=150, hint_text="Curso Nuevo")
+
     buscarAlumnoField = ft.TextField(text_align=ft.TextAlign.RIGHT, width=150, hint_text="Nombre")
     lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
     t = ft.Tabs(
@@ -26,23 +33,6 @@ def main(page: ft.Page):
         'password': '',
         'database': 'escueladb',
     }
-    
-    # Función para abrir un diálogo con información del alumno
-    def open_dlg(e):
-        alumno = buscar_alumno_dni(dni=buscarAlumnoField.value)
-        dlg = ft.AlertDialog(
-            title=ft.Text(f"DNI: {alumno[0][1]}, Nombre: {alumno[0][1]} {alumno[0][2]}, Promedio: {alumno[0][3]}, Curso: {alumno[0][4]}")
-        )
-        if len(alumno) > 0:
-            page.dialog = dlg
-            dlg.open = True
-            page.update()
-        else:
-            page.dialog = ft.AlertDialog(
-                title=ft.Text("No se encontró al alumno")
-            )
-            dlg.open = True
-            page.update()
 
     # Función para inscribir a un alumno en un curso
     def inscribir_alumno(e):
@@ -72,21 +62,19 @@ def main(page: ft.Page):
             if 'conn' in locals() and conn.is_connected():
                 cursor.close()
                 conn.close()
-                
-    
-    # Función para borrar un alumno de la base de datos
-    def borrar_alumno(dni):
+
+    # Función para obtener el total de alumnos en un curso específico
+    def total_alumnos_curso(cursoparam):
         try:
             # Conectar a la base de datos
             conn = mysql.connector.connect(**db_config)
             cursor = conn.cursor()
 
-            cursor.execute(f"DELETE FROM alumno WHERE dni = {dni}")
-            conn.commit()
+            # Obtener el total de alumnos
+            cursor.execute(f"SELECT * FROM alumno WHERE curso = '{cursoparam}'")
+            alumnosdb = cursor.fetchall()
 
-            print(f"Alumno borrado correctamente")
-            
-            page.update()
+            return alumnosdb
 
         except mysql.connector.Error as err:
             print(f"Error: {err}")
@@ -97,7 +85,107 @@ def main(page: ft.Page):
                 cursor.close()
                 conn.close()
 
+        
 
+    # Crear una lista de alumnos en un curso específico
+    def editAlumno(curso: str):
+        lv2 = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        count = 0
+        alumnos = total_alumnos_curso(cursoparam=curso)
+        for alumnos in alumnos:
+            print(f"{curso} ==? {alumno[4]}")
+            print(curso == alumno[4])
+            if curso == alumno[4]:
+                lv2.controls.append(ft.Column(
+                    [
+                        ft.Text(f"Alumnos en el curso: {len(alumnos)}"),
+                        ft.Container(
+                            border_radius=10,
+                            border=ft.border.all(2, ft.colors.GREY),
+                            padding=ft.padding.all(10),
+                            content=ft.Text(f"DNI: {alumno[0]}, Nombre: {alumno[1]} {alumno[2]}, Promedio: {alumno[3]}, Curso: {alumno[4]}")
+                        ),
+                        ft.IconButton(
+                            icon=ft.icons.EDIT,
+                            on_click=open_dlg_edit
+                        ),
+                    ]
+                ))
+            count += 1
+    
+        return lv2
+    
+    # Función para abrir un diálogo con información del alumno
+    def open_dlg(e):
+        alumno = buscar_alumno_dni(dni=buscarAlumnoField.value)
+        dlg = ft.AlertDialog(
+            title=ft.Text(f"DNI: {alumno[0][1]}, Nombre: {alumno[0][1]} {alumno[0][2]}, Promedio: {alumno[0][3]}, Curso: {alumno[0][4]}")
+        )
+        if len(alumno) > 0:
+            page.dialog = dlg
+            dlg.open = True
+            page.update()
+        else:
+            page.dialog = ft.AlertDialog(
+                title=ft.Text("No se encontró al alumno")
+            )
+            dlg.open = True
+            page.update()
+
+    def editar_alumno(e):
+        # Configuración de la conexión a la base de datos
+        db_config = {
+            'host': 'localhost',
+            'user': 'root',
+            'password': '',
+            'database': 'escueladb',
+        }
+        try:
+            # Conectar a la base de datos
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor()
+
+            # Inscribir al alumno en el curso
+            dni = dniFieldEdit.value
+            nombre = nombreFieldEdit.value
+            apellido = apellidoFieldEdit.value
+            promedio = promedioFieldEdit.value
+            curso = cursoFieldEdit.value
+            cursor.execute(f"UPDATE alumno SET dni = '{dni}', nombre = '{nombre}', apellido = '{apellido}', promedio = '{promedio}', curso = '{curso}' WHERE dni = '{dni}'")
+            conn.commit()
+
+            print(f"Alumno actualizado correctamente")
+
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+
+        finally:
+            # Cerrar la conexión a la base de datos
+            if 'conn' in locals() and conn.is_connected():
+                cursor.close()
+                conn.close()
+
+    def open_dlg_edit(e):
+        dlg_content = ft.Column(
+            [
+                dniFieldEdit,
+                nombreFieldEdit,
+                apellidoFieldEdit,
+                promedioFieldEdit,
+                cursoFieldEdit,
+                ft.Row(
+                    [
+                        ft.ElevatedButton(text="Guardar", on_click=editar_alumno)
+                    ]
+                )
+            ]
+        ),
+        dlg = ft.AlertDialog(
+            title=ft.Text(f"Editar alumno con DNI: {dniFieldEdit.value}"),
+            content=dlg_content
+        )
+        dlg.open = True
+        page.update()
     # Función para obtener el total de alumnos
     def total_alumnos():
         try:
@@ -142,27 +230,7 @@ def main(page: ft.Page):
 
         return alumnos
 
-    # Función para obtener el total de alumnos en un curso específico
-    def total_alumnos_curso(curso):
-        try:
-            # Conectar a la base de datos
-            conn = mysql.connector.connect(**db_config)
-            cursor = conn.cursor()
-
-            # Obtener el total de alumnos
-            cursor.execute(f"SELECT * FROM alumno WHERE curso = '{curso}'")
-            alumnos = cursor.fetchall()
-
-        except mysql.connector.Error as err:
-            print(f"Error: {err}")
-
-        finally:
-            # Cerrar la conexión a la base de datos
-            if 'conn' in locals() and conn.is_connected():
-                cursor.close()
-                conn.close()
-
-        return alumnos
+    
 
     # Función para obtener el total de cursos
     def total_cursos():
@@ -215,11 +283,10 @@ def main(page: ft.Page):
     cursos = total_cursos()
     count = 0
     for curso in cursos:
-        alumnosCurso = total_alumnos_curso(curso[0])
         t.tabs.append(ft.Tab(
             text=curso[0],
             icon=ft.icons.TABLE_CHART,
-            content=cursoPage(alumnos=alumnosCurso, curso=curso[0])
+            content=editAlumno(curso)
         ))
 
     alumnos = total_alumnos()
@@ -238,7 +305,6 @@ def main(page: ft.Page):
                     icon_color="red400",
                     icon_size=20,
                     tooltip="",
-                    on_click=borrar_alumno(dni=alumno[0])
                 ),
                 ft.IconButton(
                     icon=ft.icons.EDIT,
@@ -257,34 +323,6 @@ def main(page: ft.Page):
     page.add(content)
 
 
-def cursoPage(alumnos: List, curso):
-    # Crear una lista de alumnos en un curso específico
-    lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
-    count = 0
-    for alumno in alumnos:
-        print(f"{curso} ==? {alumno[4]}")
-        print(curso == alumno[4])
-        if curso == alumno[4]:
-            lv.controls.append(ft.Column(
-                [
-                    ft.Container(
-                        border_radius=10,
-                        border=ft.border.all(2, ft.colors.GREY),
-                        padding=ft.padding.all(10),
-                        content=ft.Text(f"DNI: {alumno[0]}, Nombre: {alumno[1]} {alumno[2]}, Promedio: {alumno[3]}, Curso: {alumno[4]}")
-                    ),
-                ]
-            ))
-        count += 1
-        
-    # Contenido de la página de curso
-    content = ft.Column(
-        [
-            lv
-        ]
-    )
-
-    return content
 
 # Iniciar la aplicación con la clase principal
 ft.app(target=main)
